abf1e424b9877987ece2e657c7d897ae2a09551c
jangwoo0305 <tjwkddn970305@gmail.com>
2026-01-19 16:23:39 +0900

    플레이어 이동 속도 및 중력 처리 개선

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -6,26 +6,21 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] public float edgeOffset = 0.3f;
-    [SerializeField] public float jumpHeight = 1.2f;
-    [SerializeField] public float jumpDuration = 0.5f;
-    
-    bool isJumping = false;
-
-    int jumpCount = 0;
-    const int maxJumpCount = 2;
-    
-    private float jumpOffset = 0f; // 현재 공중 높이 (절대값)
-    float targetJumpOffset = 0f; // 이번 점프의 목표 놓이
-
-    private Vector3 basepos; // 점프 제외한 기준 위치
+    [SerializeField] float cornerBlendDistance = 0.5f;
+    [SerializeField] public float jumpPower = 8f;
     
     public float speed = 3f;
+    Vector3 velocity; // 현재 이동 속도 (누적됨)
+    Vector3 gravityDir; // 현재 중력 방향 (edge 기준) 즉, 케릭터가 끌려가야하는 방향
+    private float gravityPower = 20f;
+    bool isGrounded;
+    bool isJumping;
+    
     private SpriteRenderer _spriteRend;
     Camera _cam;
     Animator _anim;
     float minX, minY, maxX, maxY;
     
-
 
     void Awake()
     {
@@ -43,6 +38,9 @@ public class PlayerMove : MonoBehaviour
         minY = bottomLeft.y + edgeOffset;
         maxX = topRight.x - edgeOffset;
         maxY = topRight.y - edgeOffset;
+        
+        gravityDir = Vector3.down;
+        velocity = Vector3.zero;
     }
 
     private enum Edge
@@ -56,137 +54,198 @@ public class PlayerMove : MonoBehaviour
     
     void Update()
     {
-        MoveEdge();
-        if (Input.GetKeyDown(KeyCode.Space))
+        ResolveGrounded(); // 1. 바닥 판단
+        HandleJumpInput();
+        ApplyGravity(); // 2. 공중일 때만 중력
+        ApplyMovement(); // 3. 이동
+        CheckCornerAndChangeGravity();
+    }
+
+    void ApplyGravity()
+    {
+        Vector3 newGravityDir = GetBlendedGravityDir();
+
+        if (newGravityDir != gravityDir)
         {
-            InputJump();
+            Quaternion rot = Quaternion.FromToRotation(gravityDir, newGravityDir);
+            velocity = rot * velocity;
+        }
+        
+        gravityDir = newGravityDir;
+        
+        if (isGrounded)
+            return;
+
+        velocity += gravityDir * gravityPower * Time.deltaTime;
+    }
+
+    void ApplyMovement()
+    {
+        Vector3 edgeMove = GetEdgeMoveDirection() * speed * Time.deltaTime;
+        transform.position += edgeMove;
+        
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    void HandleJumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
         }
     }
 
-    public void InputJump()
+    bool IsGrounded()
     {
-        if (jumpCount >= maxJumpCount) return;
-        
-        jumpCount ++;
-        
-        float maxTotalHeight = jumpHeight * maxJumpCount;
-        
-        targetJumpOffset = Mathf.Min(maxTotalHeight, jumpOffset+jumpHeight);
-        
-        StopAllCoroutines();
-        StartCoroutine(JumpCoroutine());
-        
-        _anim.SetBool("IsJumping", true);
+        Vector3 pos = transform.position;
+
+        switch (currentEdge)
+        {
+            case Edge.Bottom:
+                return pos.y <= minY;
+            case Edge.Right:
+                return pos.x >= maxX;
+            case Edge.Top:
+                return pos.y >= maxY;
+            case Edge.Left: 
+                return pos.x <= minX;
+        }
+        return false;
     }
 
-    IEnumerator JumpCoroutine()
+    void ResolveGrounded()
     {
-        isJumping = true;
-       
-        float startOffset = jumpOffset; 
-        float halfTime = jumpDuration * 0.5f;
-        float t = 0f;
-
-        while (t < halfTime)
-        {
-            t += Time.deltaTime;
-            float ratio  = t / halfTime;
-            jumpOffset = Mathf.Lerp(startOffset, targetJumpOffset, ratio);
-            yield return null;
-        }
-
-        t = 0f;
-
-        while (t < halfTime)
-        {
-            t += Time.deltaTime;
-            float ratio = t / halfTime;
-            jumpOffset = Mathf.Lerp(targetJumpOffset, 0f, ratio);
-            yield return null;
-        }
-        jumpOffset = 0f;
-        isJumping = false;
+        bool wasGrounded = isGrounded;
+        isGrounded = IsGrounded();
+        if (!isGrounded) return;
         
-        jumpCount = 0;
-        _anim.SetBool("IsJumping", false);    
+        Vector3 gravityVelocity = Vector3.Project(velocity, gravityDir);
+        velocity -= gravityVelocity;
         
+        Vector3 pos = transform.position;
+        switch (currentEdge)
+        {
+            case Edge.Bottom: pos.y = minY; break;
+            case Edge.Right: pos.x = maxX;break;
+            case Edge.Top: pos.y = maxY; break;
+            case Edge.Left: pos.x = minX; break;
+        }
+        transform.position = pos;
+
+        if (!wasGrounded)
+        {
+            isJumping = false;
+            _anim.SetBool("IsJumping", false);
+        }
     }
 
     void ChangeEdge(Edge nextEdge)
     {
         currentEdge = nextEdge;
-        UpdateVisualByEdge();
-    }
-    
-    public void MoveEdge()
-    {
+
+        // 위치 강제 고정
         Vector3 pos = transform.position;
-        float move = speed * Time.deltaTime;
 
         switch (currentEdge)
         {
             case Edge.Bottom:
-                pos.x += move;
                 pos.y = minY;
-
-                if (pos.x > maxX)
-                {
-                    pos.x = maxX;
-                    ChangeEdge(Edge.Right);
-                }
                 break;
-            
             case Edge.Right:
-                pos.y += move;
                 pos.x = maxX;
-
-                if (pos.y > maxY)
-                {
-                    pos.y = maxY;
-                    ChangeEdge(Edge.Top);
-                }
                 break;
-            
             case Edge.Top:
-                pos.x -= move;
                 pos.y = maxY;
-
-                if (pos.x < minX)
-                {
-                    pos.x = minX;
-                    ChangeEdge(Edge.Left);
-                }
                 break;
-            
             case Edge.Left:
-                pos.y -= move;
                 pos.x = minX;
-
-                if (pos.y < minY)
-                {
-                    pos.y = minY;
-                    ChangeEdge(Edge.Bottom);
-                }
                 break;
         }
-        
-        basepos = pos;
 
-        pos = basepos + GetJumpDirection() * jumpOffset;
         transform.position = pos;
-    }
 
-    Vector3 GetJumpDirection()
+        UpdateVisualByEdge();
+    }
+    Vector3 GetEdgeMoveDirection()
     {
         switch (currentEdge)
         {
-            case Edge.Bottom: return Vector3.up;
-            case Edge.Right: return Vector3.left;
-            case Edge.Top: return Vector3.down;
-            case Edge.Left: return Vector3.right;
+            case Edge.Bottom: 
+                return Vector3.right;
+            case Edge.Right: 
+                return Vector3.up;
+            case Edge.Top: 
+                return Vector3.left;
+            case Edge.Left: 
+                return Vector3.down;
         }
         return Vector3.zero;
     }
+
+    void CheckCornerAndChangeGravity()
+    {
+        Vector3 pos = transform.position;
+
+        if (currentEdge == Edge.Bottom && pos.x >= maxX)
+        {
+            ChangeEdge(Edge.Right);
+        }
+        else if (currentEdge == Edge.Right && pos.y >= maxY)
+        {
+            ChangeEdge(Edge.Top);
+        }
+        else if (currentEdge == Edge.Top && pos.x <= minX)
+        {
+            ChangeEdge(Edge.Left);
+        }
+        else if (currentEdge == Edge.Left && pos.y <= minY)
+        {
+            ChangeEdge(Edge.Bottom);
+        }
+    }
+
+    Vector3 GetBlendedGravityDir()
+    {
+        Vector3 pos = transform.position;
+
+        switch (currentEdge)
+        {
+            case Edge.Bottom:
+                if (pos.x > maxX - cornerBlendDistance)
+                {
+                    float t = Mathf.InverseLerp(maxX - cornerBlendDistance,maxX, pos.x);
+                    return Vector3.Lerp(Vector3.down, Vector3.right, t).normalized;
+                }
+                return  Vector3.down;
+            
+            case Edge.Right:
+                if (pos.y > maxY - cornerBlendDistance)
+                {
+                    float t = Mathf.InverseLerp(maxY - cornerBlendDistance, maxY, pos.y);
+                    return Vector3.Lerp(Vector3.right, Vector3.up, t).normalized;
+                }
+                return  Vector3.right;
+            
+            case Edge.Top:
+                if (pos.x < minX + cornerBlendDistance)
+                {
+                    float t = Mathf.InverseLerp(minX + cornerBlendDistance, minX, pos.x);
+                    return Vector3.Lerp(Vector3.up, Vector3.left, t).normalized;
+                }
+                return  Vector3.up;
+            
+            case Edge.Left:
+                if (pos.y < minY + cornerBlendDistance)
+                {
+                    float t = Mathf.InverseLerp(minY + cornerBlendDistance, minY, pos.y);
+                    return Vector3.Lerp(Vector3.left, Vector3.down, t).normalized;
+                }
+                return  Vector3.left;
+        }
+        return gravityDir;
+    }
+
+
     public void UpdateVisualByEdge()
     {
         float zRotation = 0f;
@@ -210,6 +269,16 @@ public class PlayerMove : MonoBehaviour
                 break;
         }
         transform.localRotation = Quaternion.Euler(0f, 0f, zRotation);
+    }
+
+    void Jump()
+    {
+        isGrounded = false;
+        isJumping = true;
+        
+        velocity += -gravityDir * jumpPower;
+        
+        _anim.SetBool("IsJumping", true);
     }
     
 }
